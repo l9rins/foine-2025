5a854d8d22cc3b07e91d33579984fd96e23943de
Marquee <lorenzbarangan112@gmail.com>
2025-12-03 19:59:22 +0800

    Add @CrossOrigin to AuthController

--- src/main/java/com/foine/controller/AuthController.java
+++ src/main/java/com/foine/controller/AuthController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "https://foine-fyt15eg5z-l9rins-projects.vercel.app", allowCredentials = "true")
 public class AuthController {
 
     private final UserRepository userRepository;
